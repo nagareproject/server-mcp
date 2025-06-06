52ed3a8379f01d81694d31928685965c7c353fde
Alain Poirier <alain.poirier@net-ng.com>
2025-06-06 17:08:32 +0200

    All requests / responses handlers moved to the `Client` class

--- src/nagare/server/mcp/application.py
+++ src/nagare/server/mcp/application.py
@@ -8,21 +8,63 @@
 # --
 
 import json
+import time
 import uuid
 import queue
+import logging
 import threading
+import collections
 from base64 import b64encode
 from functools import reduce, partial
+from itertools import dropwhile
 
 from nagare.services.router import route_for
 from nagare.services.plugins import Plugins
 from nagare.server.http_application import RESTApp
 
 
-class SSEClient:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
+class Client:
+    CLEANUP_PERIODICITY = 10
+    LOGGING_LEVELS = {
+        name: i for i, name in enumerate('debug info notice warning error critical alert emergency'.split())
+    }
+
+    def __init__(self, id, parent_logger, rpc_exports, chunk_size):
+        self.id = id
+        self.logger = logging.getLogger(parent_logger.name + '.client.' + self.id)
+        self.chunk_size = chunk_size
+
+        self.logging_level = self.LOGGING_LEVELS['error']
+        self.roots = set()  # Roots sent by the client
+        self.client_capabilities = {}
+
         self.channel = queue.Queue()
+        self.request_id = 0
+        self.last_message_sent = self.last_cleanup = time.time()
+        self.response_callbacks = collections.OrderedDict()
+
+        # RPC namespaces
+        self.rpc_exports = rpc_exports | {
+            'notifications': {'initialized': self.on_initialized, 'roots': {'list_changed': self.on_roots_changed}},
+            # 'ping': self.ping,  # noqa: ERA001
+            'logging': {'setLevel': self.set_logging_level},
+            'completion': {'complete': self.complete},
+        }
+
+    def cleanup(self, ping_timeout):
+        now = time.time()
+
+        if now > (self.last_message_sent + ping_timeout):
+            self.send('message', json.dumps({'jsonrpc': '2.0', 'method': 'ping'}).encode('utf-8'))
+
+        if now > (self.last_cleanup + self.CLEANUP_PERIODICITY):
+            self.last_cleanup = now
+
+            self.response_callbacks = collections.OrderedDict(
+                dropwhile(
+                    lambda response: (now > response[1][0] + self.CLEANUP_PERIODICITY), self.response_callbacks.items()
+                )
+            )
 
     def start_sending_loop(self, start_response, ping_timeout):
         send = start_response('200 OK', [('Content-Type', 'text/event-stream'), ('Cache-Control', 'no-cache')])
@@ -31,12 +73,11 @@ class SSEClient:
         while True:
             try:
                 try:
-                    # Wait for data with a timeout (for ping mechanism).
-                    type_, data = self.channel.get(timeout=ping_timeout)
+                    # Wait for data with a timeout (for cleanup mechanism).
+                    type_, data = self.channel.get(timeout=1)
                 except queue.Empty:
-                    # If timeout occurs (no data), send a ping message.
-                    type_ = 'message'  # Pings are sent as standard messages.
-                    data = json.dumps({'jsonrpc': '2.0', 'method': 'ping'}).encode('utf-8')
+                    self.cleanup(ping_timeout)
+                    continue
 
                 # Send the retrieved data (or ping) as an SSE event.
                 header = b'id: %d\nevent: %s\ndata: ' % (id_, type_.encode('ascii'))
@@ -50,6 +91,8 @@ class SSEClient:
                     for chunk in data:
                         send(chunk)
                     send(b'\n\n')
+
+                self.last_message_sent = time.time()
             finally:
                 # If the data object has a close method (like a file stream), call it.
                 getattr(data, 'close', lambda: None)()
@@ -61,84 +104,15 @@ class SSEClient:
             self.channel.put((data_type, data))
 
     def __repr__(self):
-        return 'SSE client <{}>'.format(self.id)
+        return 'Client {}'.format(self.id)
 
+    def create_rpc_request(self, method, response_callback, **params):
+        self.request_id += 1
+        self.response_callbacks[self.request_id] = (time.time(), response_callback)
 
-class MCPApp(RESTApp):
-    CONFIG_SPEC = RESTApp.CONFIG_SPEC | {
-        'server_name': 'string(default="Nagare MCPServer")',
-        'version': 'string(default=None)',
-        'ping_timeout': 'integer(default=5)',
-        # Define the chunk size for streaming data.
-        # The size must be a multiple of 3 to ensure that base64
-        # encoding doesn't add padding characters ('=') within
-        # the stream, only potentially at the very end.
-        'chunk_size': 'integer(default={})'.format(10 * 1024 + 2),
-    }
-
-    LOGGING_LEVELS = {
-        'debug': 0,
-        'info': 1,
-        'notice': 2,
-        'warning': 3,
-        'error': 4,
-        'critical': 5,
-        'alert': 6,
-        'emergency': 7,
-    }
-
-    capabilities = Plugins().load_plugins('capabilities', entry_points='nagare.mcp.capabilities')
-
-    def __init__(self, name, dist, server_name, version, ping_timeout, chunk_size, services_service, **config):
-        services_service(
-            super().__init__,
-            name,
-            dist,
-            server_name=server_name,
-            version=version,
-            ping_timeout=ping_timeout,
-            chunk_size=chunk_size,
-            **config,
-        )
-
-        self.server_name = server_name
-        self.version = version or dist.version  # Use provided version or distribution version.
-        self.ping_timeout = ping_timeout
-        self.chunk_size = chunk_size
-        self.services = services_service
-        self.logging_level = self.LOGGING_LEVELS['error']
-        self.client_capabilities = {}  # Capabilities sent by the client
-        self.roots = set()  # Roots sent by the client
-        self.response_handler = None
-
-        self.sseclients_lock = threading.Lock()
-        self.sseclients = {}
-
-        # RPC namespaces
-        self.rpc_exports = {name: capability.rpc_exports for name, capability in self.capabilities.items()} | {
-            'initialize': self.initialize,
-            'notifications': {'initialized': self.on_initialized, 'roots': {'list_changed': self.on_roots_changed}},
-            'ping': self.ping,
-            'logging': {'setLevel': self.set_logging_level},
-            'completion': {'complete': self.complete},
-        }
-
-    @classmethod
-    def decorators(cls):
-        return {
-            name: lambda *args, _d=decorator, _c=capability, **kw: lambda f: _d(_c, f, *args, **kw)
-            for capability in cls.capabilities.values()
-            for name, decorator in capability.decorators()
-        }
-
-    @staticmethod
-    def set_response_body(_, data):
-        return data
-
-    @staticmethod
-    def create_rpc_request(method, request_id=0, **params):
         return json.dumps(
-            {'jsonrpc': '2.0', 'id': request_id, 'method': method, 'params': params}, separators=(',', ':')
+            {'jsonrpc': '2.0', 'id': self.request_id, 'method': method, 'params': params},
+            separators=(',', ':'),
         ).encode('utf-8')
 
     @staticmethod
@@ -171,44 +145,25 @@ class MCPApp(RESTApp):
 
         yield b']}}'
 
-    def handle_request(self, chain, response=None, start_response=None, stdin=None, **params):
-        if stdin:
-            response = self.handle_json_rpc('<stdio>', json.loads(stdin))
-        else:
-            response.status_code = 202
-            response.start_response = start_response
-
-            sseclient, data = super().handle_request(chain, response=response, start_response=start_response, **params)
-            sseclient.send('message', data)
-
-        return response
-
-    def create_sseclient(self):
-        client = SSEClient()
-        with self.sseclients_lock:
-            self.sseclients[client.id] = client
-
-        return client
-
-    def disconnect_sseclient(self, client):
-        with self.sseclients_lock:
-            self.sseclients.pop(client.id, None)
-
-    def handle_json_rpc(self, client, request):
+    def handle_json_rpc(self, request, services_service):
         if method := request.get('method', ''):
             params = request.get('params', {})
 
-            self.logger.debug("Calling JSON-RPC method '%s' with %r, from %r", method, params, client)
-            return self.services(self.invoke, method.replace('.', '/').split('/'), request.get('id'), **params)
+            self.logger.debug("Calling JSON-RPC method '%s' with %r", method, params)
+            return services_service(self.invoke, method.replace('.', '/').split('/'), request.get('id'), **params)
 
         if error := request.get('error'):
-            self.logger.error('From %r: %s', client, error)
+            self.logger.error(error)
 
-        elif result := request.get('result'):
-            self.response_handle, response_handler = None, self.response_handler
-            response_handler(**result)
+        elif 'result' in request:
+            self.handle_response(**request)
 
         return None
+
+    def handle_response(self, id, result, **kw):
+        _, callback = self.response_callbacks.pop(id, (None, None))
+        if callback is not None:
+            callback(**result)
 
     # --- JSON-RPC Method Handlers ---
 
@@ -218,25 +173,14 @@ class MCPApp(RESTApp):
 
         return services_service(f, self, *args, **kw) if callable(f) else None
 
-    @staticmethod
-    def initialize(self, request_id, capabilities, **params):
-        self.client_capabilities = capabilities
-
-        return self.create_rpc_response(
-            request_id,
-            {
-                'protocolVersion': '2024-11-05',
-                'serverInfo': {'name': self.server_name, 'version': self.version},
-                'capabilities': {'roots': {}, 'completion': {}, 'logging': {}}
-                | {name: capability.infos for name, capability in self.capabilities.items() if capability},
-            },
-        )
+    def initialize(self, client_capabilities):
+        self.client_capabilities = client_capabilities
 
     def list_roots(self):
-        self.response_handler = self.on_roots_received
-        return self.create_rpc_request('roots/list')
+        return self.create_rpc_request('roots/list', self.on_roots_received)
 
     def on_roots_received(self, roots):
+        self.logger.debug('Roots received %r', roots)
         self.roots = {root['uri'] for root in roots}
 
     @staticmethod
@@ -257,11 +201,11 @@ class MCPApp(RESTApp):
     def send_log(self, logger, level, data):
         if self.LOGGING_LEVELS[level] >= self.logging_level:
             return self.create_rpc_request('notifications/message', leve=level, logger=logger, data=data)
-    """
 
     @staticmethod
     def ping(self, client, request_id, **params):
-        client.send_json(request_id, {})
+        self.send_json(request_id, {})
+    """
 
     @staticmethod
     def complete(self, *args, argument, ref, services_service, **params):
@@ -270,15 +214,103 @@ class MCPApp(RESTApp):
         )
 
 
+class MCPApp(RESTApp):
+    CONFIG_SPEC = RESTApp.CONFIG_SPEC | {
+        'server_name': 'string(default="Nagare MCPServer")',
+        'version': 'string(default=None)',
+        'ping_timeout': 'integer(default=5)',
+        # Define the chunk size for streaming data.
+        # The size must be a multiple of 3 to ensure that base64
+        # encoding doesn't add padding characters ('=') within
+        # the stream, only potentially at the very end.
+        'chunk_size': 'integer(default={})'.format(10 * 1024 + 2),
+    }
+
+    capabilities = Plugins().load_plugins('capabilities', entry_points='nagare.mcp.capabilities')
+
+    def __init__(self, name, dist, server_name, version, ping_timeout, chunk_size, services_service, **config):
+        services_service(
+            super().__init__,
+            name,
+            dist,
+            server_name=server_name,
+            version=version,
+            ping_timeout=ping_timeout,
+            chunk_size=chunk_size,
+            **config,
+        )
+
+        self.server_name = server_name
+        self.version = version or dist.version  # Use provided version or distribution version.
+        self.ping_timeout = ping_timeout
+        self.services = services_service
+        self.clients_lock = threading.Lock()
+        self.clients = {}
+
+        self.create_client = partial(
+            Client,
+            parent_logger=self.logger,
+            rpc_exports={name: capability.rpc_exports for name, capability in self.capabilities.items()}
+            | {'initialize': self.initialize},
+            chunk_size=chunk_size,
+        )
+
+    @classmethod
+    def decorators(cls):
+        return {
+            name: lambda *args, _d=decorator, _c=capability, **kw: lambda f: _d(_c, f, *args, **kw)
+            for capability in cls.capabilities.values()
+            for name, decorator in capability.decorators()
+        }
+
+    @staticmethod
+    def set_response_body(_, data):
+        return data
+
+    def handle_request(self, chain, response=None, start_response=None, stdin=None, **params):
+        if stdin:
+            stdio_client = self.clients.get('stdio')
+            if stdio_client is None:
+                stdio_client = self.clients['stdio'] = self.create_client('stdio')
+
+            response = self.services(stdio_client.handle_json_rpc, json.loads(stdin))
+        else:
+            response.status_code = 202
+            response.start_response = start_response
+
+            sseclient, data = super().handle_request(chain, response=response, start_response=start_response, **params)
+            sseclient.send('message', data)
+
+        return response
+
+    def initialize(self, client, request_id, capabilities, **params):
+        client.initialize(capabilities)
+
+        return client.create_rpc_response(
+            request_id,
+            {
+                'protocolVersion': '2024-11-05',
+                'serverInfo': {'name': self.server_name, 'version': self.version},
+                'capabilities': {'roots': {}, 'completion': {}, 'logging': {}}
+                | {name: capability.infos for name, capability in self.capabilities.items() if capability},
+            },
+        )
+
+
 # --- Route Handlers ---
 
 
 @route_for(MCPApp)
 def create_channel(self, url, method, request, response):
-    client = self.create_sseclient()
+    client_id = str(uuid.uuid4())
+    client = self.create_client(client_id)
+
+    with self.clients_lock:
+        self.clients[client_id] = client
+
     self.logger.debug('%r created', client)
 
-    endpoint_url = request.create_redirect_url() + client.id
+    endpoint_url = request.create_redirect_url() + client_id
     client.send('endpoint', endpoint_url.encode('utf-8'))
 
     try:
@@ -289,13 +321,14 @@ def create_channel(self, url, method, request, response):
         self.logger.error('Error sending data to %r: %r', e)
         raise
     finally:
-        self.disconnect_sseclient(client)
+        with self.clients_lock:
+            self.clients.pop(client_id, None)
 
     return [client, None]
 
 
 @route_for(MCPApp, '{client_id:[a-f0-9-]+}', 'POST')
 def handle_json_rpc(self, url, method, request, response, client_id):
-    client = self.sseclients[client_id]
+    client = self.clients[client_id]
 
-    return [client, self.handle_json_rpc(client, request.json_body)]
+    return [client, self.services(client.handle_json_rpc, request.json_body)]
